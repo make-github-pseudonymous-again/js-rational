6b26c0e6b316da22fb62dd0d83d6e1712b91fcd2
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-04 18:33:25 +0200

    :mag: test: Extract fixtures.

--- test/src/core.js
+++ test/src/core.js
@@ -1,11 +1,9 @@
 import test from 'ava';
 import { _add , _sub , _mul , _div , _pow , _cmp , _cmp_no_bounds , _simplify } from '../../src';
 
-import int from 'int' ;
-import BN from 'bn.js' ;
-import { ZZ } from '@aureooms/js-integer' ;
-
 const GOOGOL = '10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000' ;
+
+import { ALU } from './_fixtures' ;
 
 function binary ( t , alu , [ [ _x , _y , factory ] , a , b , c , d , e ] ) {
 
@@ -67,65 +65,6 @@ function unary ( t , alu , [ [ _x , _y , factory ] , a , b , e ] ) {
 unary.title = ( _ , alu , [ [ name , impl ] , a , b , e ] ) => {
 	return `${name}<${impl.name}, ${alu.name}> ${name}(${a}/${b}) = ${e}` ;
 } ;
-
-const ALU = [
-	{
-		name : 'int',
-		add : (a, b) => a.add(b),
-		sub : (a, b) => a.sub(b),
-		mul : (a, b) => a.mul(b),
-		div : (a, b) => a.div(b),
-		reg : x => int(x),
-		str : x => x.toString(),
-		jz  : x => x.eq(0),
-		lt0 : x => x.lt(0),
-		cmp  : (a,b) => a.cmp(b),
-		neg : x => x.neg(),
-		sgn : x => x.cmp(0),
-		divmod : (a,b) => [a.div(b), a.mod(b)],
-		pown : (x,n) => x.pow(n),
-	},
-	{
-		name : 'bn.js',
-		add : (a, b) => a.add(b),
-		sub : (a, b) => a.sub(b),
-		mul : (a, b) => a.mul(b),
-		div : (a, b) => a.div(b),
-		reg : x => new BN(x),
-		str : x => x.toString(),
-		jz  : x => x.eqn(0),
-		lt0 : x => x.ltn(0),
-		cmp  : (a,b) => a.cmp(b),
-		sgn : x => x.cmpn(0),
-		neg : x => x.neg(),
-		divmod : (a,b) => {
-			const { div , mod } = a.divmod(b) ;
-			return [div, mod] ;
-		} ,
-		egcd : (a,b) => {
-			const gcd = a.gcd(b) ;
-			return { u: b.div(gcd), v: a.div(gcd) } ;
-		} ,
-		pown : (x,n) => x.pow(new BN(n)),
-	},
-	{
-		name : '@aureooms/js-integer',
-		add : (a, b) => a.add(b),
-		sub : (a, b) => a.sub(b),
-		mul : (a, b) => a.mul(b),
-		div : (a, b) => a.div(b),
-		reg : x => ZZ.from(x),
-		str : x => x.toString(),
-		jz  : x => x.iszero(),
-		lt0 : x => x.sign() < 0,
-		cmp  : (a,b) => a.cmp(b),
-		sgn : x => x.sign(),
-		neg : x => x.opposite(),
-		divmod : (a,b) => a.divmod(b),
-		egcd : (a,b) => a.egcd(b),
-		pown : (x,n) => x.pown(n),
-	}
-];
 
 const add = [ 'add' , '+' , [ _add ] , binary ] ;
 const sub = [ 'sub' , '-' , [ _sub ] , binary ] ;
