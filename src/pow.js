fa55e01b1e4a69056ba3d6c6569cf82a4b7ca154
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-04 16:53:17 +0200

    :bug: fix(pow): Allow negative exponent.

--- src/pow.js
+++ src/pow.js
@@ -2,7 +2,7 @@ export function _pow ( { pown } ) {
 
 	return function ( a , b , n ) {
 
-		return [ pown(a,n) , pown(b,n) ] ;
+		return n >= 0 ? [ pown(a,n) , pown(b,n) ] : [ pown(b,-n) , pown(a,-n) ] ;
 
 	} ;
 
